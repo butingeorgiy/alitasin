9e3126bd92a1a9b67e4fa4de3206ca238d63632e
butingeorgiy <butingeorgiy48@gmail.com>
2021-03-22 17:33:07 +0600

    init commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script type="text/javascript" src="{{ asset('js/index.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    <title>Index page</title>
+</head>
+<body>
+    <h1 class="text-red-400">It's index page!</h1>
+</body>
+</html>
