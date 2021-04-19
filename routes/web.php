9273aa4fe73454e4915a9636eeef55aa92d6cece
butingeorgiy <butingeorgiy48@gmail.com>
2021-04-19 12:09:34 +0600

    v0.0.9-prod

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
-Route::get('', 'PageController@showIndex')->name('index');
-Route::get('regions/{id}', 'PageController@showRegion')->name('region');
-Route::get('/tours/{id}', 'PageController@showTour')->name('tour');
+Route::middleware('cache.headers:private;max_age=3600')->group(function () {
+    Route::get('', 'PageController@showIndex')->name('index');
+    Route::get('regions/{id}', 'PageController@showRegion')->name('region');
+    Route::get('/tours/{id}', 'PageController@showTour')->name('tour');
 
-Route::group(['prefix' => 'admin'], function () {
-    Route::get('', 'PageController@adminIndex')->middleware('auth:5')->name('admin-index');
-    Route::get('tours', 'TourController@showAll')->middleware('auth:5')->name('tours');
-    Route::get('tours/create', 'TourController@showCreateForm')->middleware('auth:5')->name('create-form-tour');
-    Route::get('tours/update/{id}', 'TourController@showEditForm')->middleware('auth:5')->name('edit-form-tour');
+    Route::group(['prefix' => 'admin'], function () {
+        Route::get('', 'PageController@adminIndex')->middleware('auth:5')->name('admin-index');
+        Route::get('tours', 'TourController@showAll')->middleware('auth:5')->name('tours');
+        Route::get('tours/create', 'TourController@showCreateForm')->middleware('auth:5')->name('create-form-tour');
+        Route::get('tours/update/{id}', 'TourController@showEditForm')->middleware('auth:5')->name('edit-form-tour');
+    });
+
+    Route::get('cdn/images/{fileName}', 'ImageController@get')->name('get-image');
 });
-
-Route::get('cdn/images/{fileName}', 'ImageController@get')->name('get-image');
