b5bcfea6b07309d81f697c91c2f378adf0be9e85
butingeorgiy <butingeorgiy48@gmail.com>
2021-05-28 01:39:48 +0600

    fixed

--- resources/js/modules/mobileMenu/index.js
+++ resources/js/modules/mobileMenu/index.js
@@ -1,7 +1,5 @@
 import MobileMenuController from './MobileMenuController';
 
-alert('Мобильное меню было инициализоровано!');
-
 document.addEventListener('DOMContentLoaded', _ => {
     const burgerIcon = document.querySelector('.burger-menu-icon'),
           mobileMenu = document.querySelector('.mobile-menu');
