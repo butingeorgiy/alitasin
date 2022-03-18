485e03d1ebf0dadc2739f3a6ca4444b36889172f
Butin Georgiy <butingeorgiy48@gmail.com>
2022-03-18 15:17:57 +0600

    chore: remove cache files from git index

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login', 'Api\AuthenticationController@login');
     Route::post('reg', 'Api\AuthenticationController@reg');
